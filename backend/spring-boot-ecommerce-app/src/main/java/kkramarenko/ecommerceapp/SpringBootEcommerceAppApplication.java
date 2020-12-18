220a284f04ec9b602eaa43e0f1613f40e5380917
roofflex <iamrfx@gmail.com>
2020-12-18 15:48:30 +0300

    RELEASE 1.0

---
+++ backend/spring-boot-ecommerce-app/src/main/java/kkramarenko/ecommerceapp/SpringBootEcommerceAppApplication.java
@@ -0,0 +1,13 @@
+package kkramarenko.ecommerceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEcommerceAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEcommerceAppApplication.class, args);
+    }
+
+}
---
+++ backend/spring-boot-ecommerce-app/src/test/java/kkramarenko/ecommerceapp/SpringBootEcommerceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kkramarenko.ecommerceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcommerceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
